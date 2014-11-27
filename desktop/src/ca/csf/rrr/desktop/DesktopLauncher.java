64127de71cfbdd84108f84a80bbb1c59e088c62c
dominionized <dominique.begin.0@gmail.com>
2014-11-27 10:54:36 -0500

    initial commit

---
+++ desktop/src/ca/csf/rrr/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.csf.rrr.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.csf.rrr.RunRunnerRunGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunRunnerRunGame(), config);
+	}
+}
