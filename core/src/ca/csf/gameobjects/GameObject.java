21bc01a8f0ff36c4a5ac069fa5d318d1cad18483
dominionized <dominique.begin.0@gmail.com>
2014-12-02 10:24:16 -0500

    Added GameObject class, Scrollable interface and modified spritesheet

---
+++ core/src/ca/csf/gameobjects/GameObject.java
@@ -0,0 +1,51 @@
+package ca.csf.gameobjects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class GameObject {
+    protected Vector2 position;
+    protected Vector2 velocity;
+    protected Vector2 acceleration;
+    protected int width;
+    protected int height;
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2 velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector2 getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(Vector2 acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
--- core/src/ca/csf/gameobjects/Scrollable.java
+++ core/src/ca/csf/gameobjects/Scrollable.java
@@ -1,4 +1,14 @@
 package ca.csf.gameobjects;
 
-public interface Scrollable {
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class Scrollable extends GameObject{
+
+    protected Scrollable(float scrollAmount) {
+        this.scrollAmount = scrollAmount;
+    }
+
+    protected onUpdate(){
+        
+    }
 }
