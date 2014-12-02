ef43c2fff793a7ef2bb6c636613bab4d796d389e
dominionized <dominique.begin.0@gmail.com>
2014-12-02 10:46:12 -0500

    PRobleme regled

---
+++ core/src/ca/csf/gameobjects/ScrollHandler.java
@@ -0,0 +1,4 @@
+package ca.csf.gameobjects;
+
+public class ScrollHandler {
+}
--- core/src/ca/csf/gameobjects/Scrollable.java
+++ core/src/ca/csf/gameobjects/Scrollable.java
@@ -2,13 +2,32 @@ package ca.csf.gameobjects;
 
 import com.badlogic.gdx.math.Vector2;
 
-public abstract class Scrollable extends GameObject{
+public class Scrollable extends GameObject{
 
-    protected Scrollable(float scrollAmount) {
-        this.scrollAmount = scrollAmount;
+    protected boolean isScrolledLeft;
+
+    public Scrollable(float x, float y, int width, int height, float scrollSpeed) {
+        this.position = new Vector2(x,y);
+        this.velocity = new Vector2(scrollSpeed, 0);
+        this.width = width;
+        this.height = height;
+        isScrolledLeft = false;
     }
 
-    protected onUpdate(){
-        
+    public void update(float delta){
+        position.add(velocity.cpy().scl(delta));
+
+        // If the scrollable object is no longer visible
+        if (position.x + width < 0){
+            isScrolledLeft = false;
+        }
+    }
+
+    public boolean isScrolledLeft(){
+        return this.isScrolledLeft;
+    }
+
+    public float getTailX() {
+        return position.x + width;
     }
 }
