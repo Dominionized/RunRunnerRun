0a058472c7ef1dce5bd48987c371fc5ea99cf94d
dominionized <dominique.begin.0@gmail.com>
2014-12-04 09:29:55 -0500

    Added Grass.java

---
+++ core/src/ca/csf/gameobjects/Grass.java
@@ -0,0 +1,13 @@
+package ca.csf.gameobjects;
+
+public class Grass extends Scrollable {
+    public Grass(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+    }
+
+    public void onRestart(float x, float scrollSpeed) {
+        position.x = x;
+        velocity.x = scrollSpeed;
+    }
+
+}
