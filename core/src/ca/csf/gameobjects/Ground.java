6b545623d423881a591024ca6b024c50eedb683b
Alexandre Hamel <hamelbass@outlook.com>
2014-12-04 08:10:03 -0500

    I coded the ground class.

--- core/src/ca/csf/gameobjects/Ground.java
+++ core/src/ca/csf/gameobjects/Ground.java
@@ -1,4 +1,13 @@
 package ca.csf.gameobjects;
 
-public class Ground {
+public class Ground extends Scrollable {
+    public Ground(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+    }
+
+    public void onRestart(float x, float scrollSpeed) {
+        position.x = x;
+        velocity.x = scrollSpeed;
+    }
+
 }
