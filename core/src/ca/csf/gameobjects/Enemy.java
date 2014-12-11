783988c78b47615b09d427a0ef33317ec6b4203d
dominique09 <dominique.septembre@gmail.com>
2014-12-11 10:37:02 -0500

    Enemy brach created ;0

--- core/src/ca/csf/gameobjects/Enemy.java
+++ core/src/ca/csf/gameobjects/Enemy.java
@@ -1,4 +1,22 @@
 package ca.csf.gameobjects;
 
-public class Enemy {
+import com.badlogic.gdx.math.Rectangle;
+
+public class Enemy extends Scrollable implements Killable {
+    public Rectangle bouncingRectangle;
+    public Boolean isAlive;
+
+    public Enemy(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+        isAlive = true
+    }
+
+    public Rectangle getBouncingRectangle() {
+        return bouncingRectangle;
+    }
+
+    @Override
+    public void onKilled() {
+        isAlive = false;
+    }
 }
