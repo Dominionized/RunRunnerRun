7967aef59206e4204284eb1e555210a3ac1df44d
dominique09 <dominique.septembre@gmail.com>
2014-12-18 21:25:35 -0500

    Enemy test

--- core/test/EnemyTest.java
+++ core/test/EnemyTest.java
@@ -1,6 +1,6 @@
-package ca.csf.rrr.gameobjects;
-
 import junit.framework.TestCase;
+
+import ca.csf.rrr.gameobjects.Enemy;
 
 public class EnemyTest extends TestCase {
  //float x, float y, int width, int height, float scrollSpeed
