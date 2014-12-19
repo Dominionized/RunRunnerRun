503b694f518c4f22443375d9ef53298cd609ce06
dominique09 <dominique.septembre@gmail.com>
2014-12-18 22:24:15 -0500

    EnemyTest

---
+++ core/test/EnemyTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.csf.rrr.gameobjects.Enemy;
+
+import static org.junit.Assert.*;
+
+public class EnemyTest {
+    Enemy enemy;
+
+    @Before
+    public void setUp() throws Exception {
+        enemy = new Enemy(0.0f, 0.0f, 0, 0, 0.0f);
+    }
+
+    @Test
+    public void testOnKilled() throws Exception {
+        enemy.onKilled();
+        assertFalse(enemy.isAlive());
+    }
+}
