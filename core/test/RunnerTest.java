9fd9cf93b7d4247915eec5f818d24dfd8720e9a1
dominionized <dominique.begin.0@gmail.com>
2014-12-18 20:21:15 -0500

    More testing

---
+++ core/test/RunnerTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.csf.rrr.gameobjects.Runner;
+
+import static org.junit.Assert.*;
+
+public class RunnerTest {
+
+    Runner runner;
+
+
+    @Before
+    public void setUp() throws Exception {
+        runner = new Runner(0.0f, 0.0f, 0, 0);
+
+    }
+
+    @Test
+    public void testOnKick() throws Exception {
+
+        runner.onKick();
+        assertTrue(runner.isKicking());
+    }
+
+
+}
