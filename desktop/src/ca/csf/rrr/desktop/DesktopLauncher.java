2c77f33274f36c292474b22f2982dc4946a3672d
Alexandre Hamel <hamelbass@outlook.com>
2014-12-23 01:50:47 -0500

    I might have solved a bugg

--- desktop/src/ca/csf/rrr/desktop/DesktopLauncher.java
+++ desktop/src/ca/csf/rrr/desktop/DesktopLauncher.java
@@ -9,8 +9,9 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Run Runner Run";
-        config.width = 1024;
-        config.height = 500;
+        config.width = 1280;
+        config.height = 720;
         new LwjglApplication(new RunRunnerRunGame(), config);
     }
 }
+
